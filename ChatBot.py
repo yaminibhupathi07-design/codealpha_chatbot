7d63e14265df2ff3504a4c8166f1e58f56aa6c31
Bhupathi Yamini <yaminibhupathi07@gmail.com>
2026-08-18 22:24:58 +0530

    Add files via upload

---
+++ ChatBot.py
@@ -0,0 +1,21 @@
+def chatbot():
+    print("Chatbot: Hello! Type 'bye' to exit.")
+
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("Chatbot: Hi!")
+
+        elif user_input == "how are you":
+            print("Chatbot: I'm fine, thanks!")
+
+        elif user_input == "bye":
+            print("Chatbot: Goodbye!")
+            break
+
+        else:
+            print("Chatbot: Sorry, I don't understand that.")
+
+# Start the chatbot
+chatbot()
